d5e3a3de4b28a9bff2053226d14af7a0c2d3537c
Guangxuan-Xiao <xiaoguangxuan@outlook.com>
2021-06-18 22:54:11 +0800

    transpose the dense mat before spmm, slower

--- src/spmm_opt.cu
+++ src/spmm_opt.cu
@@ -1,5 +1,6 @@
 #include "spmm_opt.h"
 #include <stdio.h>
+
 __global__ void spmm_kernel_opt(int *ptr, int *idx, float *val, float *vin, float *vout, int num_v, int feat_in)
 {
     int tid = blockIdx.x * blockDim.x + threadIdx.x;
@@ -12,10 +13,11 @@ __global__ void spmm_kernel_opt(int *ptr, int *idx, float *val, float *vin, floa
         for (int i = begin; i < end; ++i)
         {
             // Transposing the vin maybe cache-friendly
-            result += vin[idx[i] * feat_in + j] * val[i];
+            result += vin[idx[i] + j * num_v] * val[i];
         }
         vout[tid * feat_in + j] = result;
     }
+
 }
 
 void SpMMOpt::preprocess(float *vin, float *vout)
@@ -31,5 +33,13 @@ void SpMMOpt::run(float *vin, float *vout)
     // dbg("TODO");
     // printf("Grid = <%d, %d, %d>\n", grid.x, grid.y, grid.z);
     // printf("Block = <%d, %d, %d>\n", block.x, block.y, block.z);
-    spmm_kernel_opt<<<grid, block>>>(d_ptr, d_idx, d_val, vin, vout, num_v, feat_in);
+    float *new_vin;
+    cudaMalloc(&new_vin, feat_in * num_v * sizeof(float));
+    cublasCreate(&handle);
+    float alpha = 1, beta = 0;
+    cublasSgeam(handle, CUBLAS_OP_T, CUBLAS_OP_N, num_v, feat_in, &alpha, vin, feat_in,
+                        &beta,
+                        nullptr, num_v,
+                        new_vin, num_v);
+    spmm_kernel_opt<<<grid, block>>>(d_ptr, d_idx, d_val, new_vin, vout, num_v, feat_in);
 }
