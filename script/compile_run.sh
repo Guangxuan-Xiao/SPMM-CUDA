4b0c2747de354ffe1e4695b665e7c6db9a778203
Guangxuan-Xiao <xiaoguangxuan@outlook.com>
2021-06-18 21:10:59 +0800

    0.0433497

--- script/compile_run.sh
+++ script/compile_run.sh
@@ -4,10 +4,5 @@ spack load cuda
 
 bash compile.sh
 
-if [ "$1" = "" ]; then
-	echo Usage: ./run.sh testcase
-	exit 1
-fi
-
-srun ~/PA4_build/test/unit_tests --dataset $1 --datadir ~/PA4/data/ --len 32
+bash run.sh $1
 
--- src/spmm_opt.cu
+++ src/spmm_opt.cu
@@ -1,19 +1,20 @@
 #include "spmm_opt.h"
 #include <stdio.h>
-__global__ void spmm_kernel_opt(int *ptr, int *idx, float *val, float *vin, float *vout, int num_v, int INFEATURE)
+__global__ void spmm_kernel_opt(int *ptr, int *idx, float *val, float *vin, float *vout, int num_v, int feat_in)
 {
     int tid = blockIdx.x * blockDim.x + threadIdx.x;
     if (tid >= num_v)
         return;
     int begin = ptr[tid], end = ptr[tid + 1];
-    for (int j = 0; j < INFEATURE; ++j)
+    for (int j = 0; j < feat_in; ++j)
     {
         float result = 0.0f;
         for (int i = begin; i < end; ++i)
         {
-            result += vin[idx[i] * INFEATURE + j] * val[i];
+            // Transposing the vin maybe cache-friendly
+            result += vin[idx[i] * feat_in + j] * val[i];
         }
-        vout[tid * INFEATURE + j] = result;
+        vout[tid * feat_in + j] = result;
     }
 }
 
@@ -28,7 +29,7 @@ void SpMMOpt::preprocess(float *vin, float *vout)
 void SpMMOpt::run(float *vin, float *vout)
 {
     // dbg("TODO");
-    printf("Grid = <%d, %d, %d>\n", grid.x, grid.y, grid.z);
-    printf("Block = <%d, %d, %d>\n", block.x, block.y, block.z);
+    // printf("Grid = <%d, %d, %d>\n", grid.x, grid.y, grid.z);
+    // printf("Block = <%d, %d, %d>\n", block.x, block.y, block.z);
     spmm_kernel_opt<<<grid, block>>>(d_ptr, d_idx, d_val, vin, vout, num_v, feat_in);
 }
